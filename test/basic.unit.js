f1387d15bfbd41738e173b67df04b90310666388
Ryan Kotzen <potz666@gmail.com>
2017-08-09 21:40:02 +0200

    Adding some tests

--- test/basic.unit.js
+++ test/basic.unit.js
@@ -1,8 +1,26 @@
-import sayHello from '../src';
+import initialise from '../src';
+import { IsRequiredError } from 'krimzen-ninja-common-errors';
+import express from 'express';
 
-describe('sayHello', () => {
-    it('returns hello', () => {
-        expect(sayHello()).toBe('Hello, World!');
-        expect(sayHello('foo')).toBe('Hello, foo!');
+describe('expressErrorHandler', () => {
+    describe('initialise', function() {
+        it('should throw an error if no options provided', () => {
+            expect(() => initialise()).toThrow(TypeError);
+        });
+        it('should throw an error if name was not provided', () => {
+            expect(() => initialise({})).toThrow(IsRequiredError);
+            expect(() => initialise({})).toThrow(/options\.name/);
+        });
+        it('should throw an error if app was not provided', () => {
+            expect(() => initialise({ name: 'asd' })).toThrow(IsRequiredError);
+            expect(() => initialise({ name: 'asd' })).toThrow(/options\.app/);
+        });
+        it('should should succeed for the happy case', () => {
+            const app = new express();
+            initialise({
+                name: 'test',
+                app
+            });
+        });
     });
 });
